0ce490da13c97121ab91592ee48995e07ee20932
Student04 <student04@HackaTruck04.local>
2023-07-19 14:42:46 -0300

    Forum geral consertado

--- projetoITNomads/Forum/ForumEspecificoView.swift
+++ projetoITNomads/Forum/ForumEspecificoView.swift
@@ -17,48 +17,50 @@ struct ForumEspecificoView: View {
     @State var nomePais: String = "Especifico"
     
     var body: some View {
-        ZStack{
-            
-            ScrollView{
-                        VStack{
-                            Text(nomePais)
-                                .font(.largeTitle.bold())
-                            
-                            
-                            ForEach(viewModelForum.chars, id: \._id){ per in
-                                if per.pais! == nomePais {
-                                    
-                                    NavigationLink(destination: RespostaView(id: per._id!)){
-                                        Text(per.tituloPergunta!)
-                                    }
-                                        .font(.title)
-                                    Text(per.userPergunta!)
-                                    Rectangle()
-                                }
+        NavigationStack{
+            ZStack{
+                
+                ScrollView{
+                    VStack{
+                        Text(nomePais)
+                            .font(.largeTitle.bold())
+                        
+                        
+                        ForEach(viewModelForum.chars, id: \._id){ per in
+                            if per.pais! == nomePais {
                                 
+                                NavigationLink(destination: RespostaView(id: per._id!)){
+                                    Text(per.tituloPergunta!)
+                                }
+                                .font(.title)
+                                Text(per.userPergunta!)
+                                Rectangle()
                             }
+                            
                         }
-                    }.onAppear(){
-                        viewModelForum.fetch()
-                }
-            
-            VStack{
-                HStack{
-                    Spacer()
-                    Button{
-                        presentSideMenu.toggle()
-                    } label: {
-                        Image(systemName: "list.dash")
-                            .resizable()
-                            .frame(width: 25, height: 20)
-                            .foregroundColor(.black)
                     }
+                }.onAppear(){
+                    viewModelForum.fetch()
                 }
                 
-                Spacer()
-                Spacer()
+                VStack{
+                    HStack{
+                        Spacer()
+                        Button{
+                            presentSideMenu.toggle()
+                        } label: {
+                            Image(systemName: "list.dash")
+                                .resizable()
+                                .frame(width: 25, height: 20)
+                                .foregroundColor(.black)
+                        }
+                    }
+                    
+                    Spacer()
+                    Spacer()
+                }
+                .padding(.horizontal, 24)
             }
-            .padding(.horizontal, 24)
         }
     }
 }
--- projetoITNomads/Forum/ForumView.swift
+++ projetoITNomads/Forum/ForumView.swift
@@ -17,47 +17,49 @@ struct ForumView: View {
     @State var nomePais: String = "Geral"
     
     var body: some View {
-        ZStack{
-            
-            ScrollView{
-                        VStack{
-                            Text(nomePais)
-                                .font(.largeTitle.bold())
-                            
-                            
-                            ForEach(viewModelForum.chars, id: \._id){ per in
-                                if nomePais == "Geral" {
-                                    NavigationLink(destination: RespostaView(id: per._id!)){
-                                        Text(per.tituloPergunta!)
-                                    }
-                                        .font(.title)
-                                    Text(per.userPergunta!)
-                                    Rectangle()
+        NavigationStack{
+            ZStack{
+                
+                ScrollView{
+                    VStack{
+                        Text(nomePais)
+                            .font(.largeTitle.bold())
+                        
+                        
+                        ForEach(viewModelForum.chars, id: \._id){ per in
+                            if nomePais == "Geral" {
+                                NavigationLink(destination: RespostaView(id: per._id!)){
+                                    Text(per.tituloPergunta!)
                                 }
-                                
+                                .font(.title)
+                                Text(per.userPergunta!)
+                                Rectangle()
                             }
+                            
                         }
-                    }.onAppear(){
-                        viewModelForum.fetch()
-                }
-            
-            VStack{
-                HStack{
-                    Spacer()
-                    Button{
-                        presentSideMenu.toggle()
-                    } label: {
-                        Image(systemName: "list.dash")
-                            .resizable()
-                            .frame(width: 25, height: 20)
-                            .foregroundColor(.black)
                     }
+                }.onAppear(){
+                    viewModelForum.fetch()
                 }
                 
-                Spacer()
-                Spacer()
+                VStack{
+                    HStack{
+                        Spacer()
+                        Button{
+                            presentSideMenu.toggle()
+                        } label: {
+                            Image(systemName: "list.dash")
+                                .resizable()
+                                .frame(width: 25, height: 20)
+                                .foregroundColor(.black)
+                        }
+                    }
+                    
+                    Spacer()
+                    Spacer()
+                }
+                .padding(.horizontal, 24)
             }
-            .padding(.horizontal, 24)
         }
     }
 }
